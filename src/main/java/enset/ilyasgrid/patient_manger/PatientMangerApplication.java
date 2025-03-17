73d69d79d1e09609df6e0dc73c9a162b871106b2
ilyasGrid <ilyasgrid17@gmail.com>
2025-03-17 09:56:56 +0000

    setting up the project and creating Patient model

---
+++ src/main/java/enset/ilyasgrid/patient_manger/PatientMangerApplication.java
@@ -0,0 +1,13 @@
+package enset.ilyasgrid.patient_manger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientMangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientMangerApplication.class, args);
+    }
+
+}
