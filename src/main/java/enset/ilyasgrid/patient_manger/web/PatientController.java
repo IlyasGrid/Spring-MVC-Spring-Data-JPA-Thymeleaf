83899486880a08f66db4b33cce1e2ce830105ad4
ilyasGrid <ilyasgrid17@gmail.com>
2025-06-09 20:11:55 +0100

    trying to merge branches

--- src/main/java/enset/ilyasgrid/patient_manger/web/PatientController.java
+++ src/main/java/enset/ilyasgrid/patient_manger/web/PatientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@Controller
+@Controller 
 @AllArgsConstructor
 public class PatientController {
     @Autowired
